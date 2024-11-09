153f57cf86a5686511c6fd448342b646f31187e7
Giorf81 <rejroberts7@gmail.com>
2024-11-09 13:12:18 +0300

    Добавьте файлы проекта.

---
+++ 6-6-2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _6_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        class User()
+        {
+            private string login;
+            private string mail;
+
+            public string Login
+            {
+                get { 
+                return login;
+                }
+                set {
+                    if (login.Length < 3)
+                        Console.WriteLine("Логин должен содержать больше двух символов");
+                    else login = value;
+                }
+            }
+
+            public string Mail
+            {
+                get { return mail;}
+                set
+                {
+                    if (mail.Contains("@"))
+                        mail = value;
+                    else
+                        Console.WriteLine("Поле не содержит @");
+
+                }
+            }
+        }
+        
+    }
+}
